42cf4ff65c39627418276fb560f8be99dc13b92f
GCoderHW <534069491@qq.com>
2018-08-03 09:46:30 +0800

    001

--- MySvrConnector.cpp
+++ MySvrConnector.cpp
@@ -66,6 +66,28 @@ void MySvrConnector::run()
 	{
 		fd_set rfds_cp = m_rfds;
 		int n = select(m_cur_maxfds, &rfds_cp, NULL, NULL, NULL);
+
+		int cur_count = 0;
+
+		if (FD_ISSET(m_listen_socket, &rfds_cp))
+		{
+			Accept();
+			++cur_count;
+			if (n <= cur_count)
+				continue;
+		}
+
+		for (std::map<unsigned int, socket_info>::const_iterator it = client_socket_map.begin(); it != client_socket_map.end(); ++it)
+		{
+
+			if (FD_ISSET(it->first, &rfds_cp))
+			{
+				Recv(it->first);
+				++cur_count;
+				if (n <= cur_count)
+					break;
+			}
+		}
 	}
 	
 }
@@ -85,7 +107,7 @@ int MySvrConnector::Bind()
 	
 	m_sockaddr.sin_family = AF_INET;
 	m_sockaddr.sin_port = htons(6666);
-	inet_pton(AF_INET, "127.0.0.1", (void*)&m_sockaddr.sin_addr);
+	inet_pton(AF_INET, INADDR_ANY, (void*)&m_sockaddr.sin_addr);
 
 	if(0 != bind(m_listen_socket, (sockaddr*)&m_sockaddr, sizeof(m_sockaddr)))
 		return -1;
@@ -99,6 +121,9 @@ int MySvrConnector::Listen()
 	if (0 != listen(m_listen_socket, 5))
 		return -1;
 	FD_SET(m_listen_socket, &m_rfds);
+	
+	printf("listen sucess!\nbegin listen port:%d, listen fd:%d\n", ntohs(m_sockaddr.sin_port), m_listen_socket);
+
 	return 0;
 }
 
@@ -114,21 +139,49 @@ int MySvrConnector::Accept(/*unsigned int nfd, const sockaddr_in* pSockaddr*/)
 	if(client_socket_map.size() >= m_max_connect)
 		return -1;
 
+	nfd = 1;
+
 	socket_info new_socket_info;
+	memset(&new_socket_info, 0, sizeof(new_socket_info));
 
+	
 	new_socket_info.socket = nfd;
-	memcpy(&new_socket_info, &clientAddr, sizeof(new_socket_info));
+	//memcpy(&new_socket_info.socket_addr, &clientAddr, sizeof(new_socket_info));
+	PCSTR p = inet_ntop(AF_INET, &clientAddr.sin_addr, new_socket_info.ip, sizeof(new_socket_info.ip));
+	if (!p)
+	{
+		printf("call inet_ntop failed!\n");
+		return 0;
+	}
 
-	client_socket_map.insert(std::make_pair(nfd, new_socket_info));
+	//new_socket_info.port = ntohs(clientAddr.sin_port);
+	
+	client_socket_map.insert(std::make_pair(new_socket_info.socket, new_socket_info));
+
+	printf("Accept from ip:%s, port:%d, fd:%d\n", new_socket_info.ip, new_socket_info.port, new_socket_info.socket);
+
 	FD_SET(nfd, &m_rfds);
 
 	return 0;
 }
 
-int MySvrConnector::Recv()
+int MySvrConnector::Recv(unsigned int nfd)
 {
+	std::map<unsigned int, socket_info>::const_iterator it_fd = client_socket_map.find(nfd);
+	if (it_fd == client_socket_map.end())
+	{
+		Close(nfd);
+		return -1;
+	}
 
-	return 0;	
+	int n = recv(nfd, m_recv_buf, sizeof(m_recv_buf), 0);
+	if (0 >= n)
+	{
+		return -1;
+	}
+	printf("recv data[%s],from ip:%s,port:%d, fd:%d\n", m_recv_buf, it_fd->second.ip, it_fd->second.port, it_fd->first);
+
+	return 0;
 }
 
 int MySvrConnector::Send()
